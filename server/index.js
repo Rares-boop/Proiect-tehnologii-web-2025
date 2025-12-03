c7f8244ee7255378e41f3286c0a7c6f797ed53ef
Rares <vladurares@gmail.com>
2025-12-03 11:17:43 +0200

    added gitignore

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import "dotenv/config"
+
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log(`Server started on port ${port}`);
