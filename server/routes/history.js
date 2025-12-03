e7dd0a9e63f5e365bbf716c9addc294c2e41f45c
vcryss99 <voicubogdan2018@yahoo.com>
2025-12-03 23:03:00 +0200

    Add history routes for fetching and creating entries

---
+++ server/routes/history.js
@@ -0,0 +1,44 @@
+import express from "express";
+import History from "../models/history.js";
+
+const historyRouter = express.Router();
+
+historyRouter.get("/bounty/:bountyId", async (req, res, next) => {
+    try {
+        const history = await History.findAll({
+            where: {
+                bountyId: req.params.bountyId
+            },
+            order: [['createdAt', 'DESC']]
+        });
+
+        if (history.length > 0) {
+            return res.status(200).json(history);
+        } else {
+            return res.status(200).json([]); 
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+historyRouter.post("/bounty/:bountyId/user/:userId", async (req, res, next) => {
+    try {
+        if (!req.body.changeDescription) {
+            return res.status(400).json({ message: "Change description is missing" });
+        }
+
+        const newEntry = await History.create({
+            changeDescription: req.body.changeDescription,
+            bountyId: req.params.bountyId, 
+            userId: req.params.userId
+        });
+
+        return res.status(201).json(newEntry);
+    } catch (err) {
+        next(err);
+    }
+});
+
+export default historyRouter;
